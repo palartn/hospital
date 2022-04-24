b7bd1569798fa77416db261a8fa94ddc03d065e5
Nael <palart@gmail.com>
2022-04-24 17:26:52 +0300

    Auth User

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -247,7 +247,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->name;}}</span>
                                 <img class="img-profile rounded-circle"
                                     src="{{asset('admin-style/img/undraw_profile.svg')}}">
                             </a>
@@ -260,10 +260,11 @@
                                 </a>
                               
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                <a  href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
+                             
                             </div>
                         </li>
 
@@ -276,7 +277,7 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    @yield('content');
+                    @yield('content')
 
                 </div>
                 <!-- /.container-fluid -->
@@ -319,7 +320,11 @@
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" class="dropdown-item" onclick="event.preventDefault();document.getElementById('logout-form').submit()" href="{{ route('logout') }}">Logout</a>
+
+                    <form action="{{route('logout')}}" method="POST" id="logout-form" >
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Admincontroller;
 
@@ -8,10 +9,8 @@ Route::get('/', function () {
     return view('home');})->name('homepage');;
 
 Route::prefix('admin')->name('admin.')->group(function(){
-Route::get('',[Admincontroller::class,'index'])->name('index');
+Route::get('',[Admincontroller::class,'index'])->name('index')->middleware('auth'); 
 });
-
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
